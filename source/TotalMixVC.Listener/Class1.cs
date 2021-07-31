b81cf1a2c4126d6efe825ed89d4c7ad403df4f76
Fotis Gimian <fgimiansoftware@gmail.com>
2021-07-31 15:55:25 +1000

    Initial commit.

---
+++ source/TotalMixVC.Listener/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TotalMixVC.Listener
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/TotalMixVC.Sender/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TotalMixVC.Sender
+{
+    public class Class1
+    {
+    }
+}
