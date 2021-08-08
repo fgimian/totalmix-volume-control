dfaa2fbf0b94eeb649dd0641387405e731a75e18
Fotis Gimian <fgimiansoftware@gmail.com>
2021-08-08 15:08:03 +1000

    Implement interfaces for the listener and sender to aid in testing.

---
+++ source/TotalMixVC.Communicator/IListener.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using OscCore;
+
+namespace TotalMixVC.Communicator
+{
+    /// <summary>
+    /// Interface defining a UDP receiver for Open Source Control (OSC) traffic.
+    /// </summary>
+    public interface IListener
+    {
+        /// <summary>
+        /// Receives an OSC packet from the endpoint configured.
+        /// </summary>
+        /// <returns>
+        /// An OSC packet which may be either a <see cref="OscBundle"/> or <see cref="OscMessage"/>.
+        /// </returns>
+        Task<OscPacket> ReceiveAsync();
+    }
+}
